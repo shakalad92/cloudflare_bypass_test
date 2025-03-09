91cedda8670dee96bdfc11de128b49e43ac16bd5
Arthur <arthur.petrosyan92@gmail.com>
2025-03-09 08:20:12 +0400

    added screenshot maker

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ with SB(uc=True, test=True, locale="en") as sb:
     url = "https://vipdrive.net/"
     sb.activate_cdp_mode(url)
     sb.uc_gui_click_captcha()
-    sb.sleep(2)
+    sb.sleep(5)
+
+    sb.save_screenshot()
