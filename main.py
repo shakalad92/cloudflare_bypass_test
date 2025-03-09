af2bb97ba73541076c9be4140ddff30745dbfa20
Arthur <arthur.petrosyan92@gmail.com>
2025-03-09 08:04:32 +0400

    testing file

---
+++ main.py
@@ -0,0 +1,7 @@
+from seleniumbase import SB
+
+with SB(uc=True, test=True, locale="en") as sb:
+    url = "https://vipdrive.net/"
+    sb.activate_cdp_mode(url)
+    sb.uc_gui_click_captcha()
+    sb.sleep(2)
