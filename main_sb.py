3b82e85ce41684fe00c9d0dac98265e8389bbbba
Arthur <arthur.petrosyan92@gmail.com>
2025-03-12 18:46:53 +0400

    testing testing

--- main_sb.py
+++ main_sb.py
@@ -1,6 +1,6 @@
 from seleniumbase import Driver
 
-driver = Driver(uc=True)
+driver = Driver(server="http://localhost:4444/wd/hub", browser="chrome", uc=True)
 url = "https://vipdrive.net"
 driver.uc_open_with_reconnect(url, 4)
 driver.uc_gui_click_captcha()
